da2f2c18ff531dd3f9ecfb111ae1f387ffd00173
Meraj-Sharif-Khan <merajkhan.mk101@gmail.com>
2025-04-22 19:53:44 +0600

    Visited feture added and image converted to webp

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const porjectSl = document.querySelectorAll(".projectTitle");
-
+const link = document.querySelectorAll("a");
+console.log(link);
 porjectSl.forEach((e, i) => {
   const title = e.innerText;
   e.innerHTML = `<h2>#${i + 1} ${title}</h2>`;
+});
+
+link.forEach((e) => {
+  e.addEventListener("click", () => {
+    e.classList.add("visited");
+  });
 });
